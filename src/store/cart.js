a5f7e9913a799b8a262c53a0f0b8b69f0123aebf
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-03 13:34:40 +0100

    More cart reducer improvements - switch to OBJECT with cartTotal and items

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,18 +1,18 @@
-const ADD_TO_CART = 'ADD_TO_CART';
+// const ADD_TO_CART = 'ADD_TO_CART';
 
-export const addToCart = price => ({
-  type: ADD_TO_CART,
-  price
-})
+// export const addToCart = price => ({
+//   type: ADD_TO_CART,
+//   price
+// })
 
-export default (state = 0, action) => {
-  switch (action.type) {
-    case ADD_TO_CART:
-      return state + action.price;
-    default:
-      return state;
-  }
-}
+// export default (state = 0, action) => {
+//   switch (action.type) {
+//     case ADD_TO_CART:
+//       return state + action.price;
+//     default:
+//       return state;
+//   }
+// }
 
 
 
@@ -37,3 +37,32 @@ export default (state = 0, action) => {
 //       return state;
 //   }
 // }
+
+
+const ADD_TO_CART = 'ADD_TO_CART';
+
+const INITIAL_STATE = {
+  cartTotal: 0,
+  items: []
+};
+
+export const addToCart = (title, price) => ({
+  type: ADD_TO_CART,
+  item: {
+    title,
+    price
+  }
+});
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        items: [...state.items, action.item],
+        cartTotal: (state.cartTotal + action.item.price)
+      };
+    default:
+      return state;
+  }
+}
