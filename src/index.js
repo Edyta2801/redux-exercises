9099d1d8ec4688301b58c35e626e2c5c9e337b34
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-03 10:59:30 +0100

    add inspector reducer

--- src/index.js
+++ src/index.js
@@ -3,24 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {combineReducers,createStore} from  'redux'
-import todos, {addTodo} from './store'
+import { combineReducers, createStore } from 'redux'
+import todos, { addTodo } from './store'
 
 
 
-const rootReducer=combineReducers({
+const rootReducer = combineReducers({
     todos
 })
 
-const store=createStore(rootReducer);
+const store = createStore(rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
-console.warn('before dispatch:',store.getState());
+console.warn('before dispatch:', store.getState());
 store.dispatch(addTodo('Go shopping'));
 store.dispatch(addTodo('Some other thing'));
 store.dispatch(addTodo('Clean the house'));
 store.dispatch(addTodo('Feed cat'));
-console.warn ('after dispatch:', store.getState())
+console.warn('after dispatch:', store.getState())
 
 
 
